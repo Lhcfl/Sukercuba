29e42004acf18329af1064d45db070152e20bac5
Lhcfl <lhcfl@outlook.com>
2025-09-05 22:51:25 +0800

    🚨 make linter happy

--- src/workers/draw-blurhash.ts
+++ src/workers/draw-blurhash.ts
@@ -3,19 +3,19 @@
  * SPDX-License-Identifier: AGPL-3.0-only
  */
 
-import { render } from 'buraha';
+import { render } from "buraha";
 
 // biome-ignore lint/suspicious/noGlobalAssign: Worker context
 onmessage = (event) => {
-  if (typeof event.data !== 'object' || event.data === null) {
-    throw new Error('draw-blurhash: invalid data');
+  if (typeof event.data !== "object" || event.data === null) {
+    throw new Error("draw-blurhash: invalid data");
   }
   const { id, hash } = event.data;
-  if (!(typeof id === 'string')) {
-    throw new Error('draw-blurhash: invalid id');
+  if (!(typeof id === "string")) {
+    throw new Error("draw-blurhash: invalid id");
   }
-  if (!(typeof hash === 'string')) {
-    throw new Error('draw-blurhash: invalid hash');
+  if (!(typeof hash === "string")) {
+    throw new Error("draw-blurhash: invalid hash");
   }
   const canvas = new OffscreenCanvas(64, 64);
   render(hash, canvas);
